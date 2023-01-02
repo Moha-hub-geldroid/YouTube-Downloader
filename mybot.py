7b0cde1f3a02f1f5e1a164dfd2e290b9b7bbe9c3
Moha-hub-geldroid <81656341+Moha-hub-geldroid@users.noreply.github.com>
2023-01-02 23:27:22 +0300

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -15,7 +15,7 @@ bot = telebot.TeleBot("5693306698:AAHfNtFFdw_kTXO-NMlQcaDAKGMXWBkUIcI")
 # start message 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, text = "Welcome To YouTube Downloader\n\nOwner: @sirr_b52")
+    bot.send_message(message.chat.id, text = "Welcome To YouTube Downloader ✅\n\nOwner: @sirr_b52")
 @bot.message_handler(commands=["commands"])
 def commands(message):
     bot.send_message(message.chat.id,text="/start ==> getting start with this bot !\n\n/help ==> to get help in how to use this bot\n\n/commands ==> to show all commands \n\n/youtube_help ==> to get help in how to use youtube tools\n\n/youtube <VIDEO LINK> ==> to download a youtube video 'mp4 extension'\n\n/youtube_audio <VIDEO LINK> ==> to download youtube video's audio 'mp3 extension'\n\n/instagram ==> download any instagram video or photo (no reels or IGTV)")
