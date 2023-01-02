e72edaf7f798fedfd38a1574e52392608f8bbf41
Moha-hub-geldroid <81656341+Moha-hub-geldroid@users.noreply.github.com>
2023-01-02 23:36:00 +0300

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -10,12 +10,12 @@ import json
 ###################################################################
 
 
-bot = telebot.TeleBot("5693306698:AAHfNtFFdw_kTXO-NMlQcaDAKGMXWBkUIcI")
+bot = telebot.TeleBot("5833750101:AAEF4r6FTnHarUvwjBF7PNdQ7v1X10Oa6CQ")
 
 # start message 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, text = "Welcome To YouTube Downloader ✅\n\nOwner: @sirr_b52")
+    bot.send_message(message.chat.id, text = "Welcome To YouTube Downloader\n\nOwner: @sirr_b52")
 @bot.message_handler(commands=["commands"])
 def commands(message):
     bot.send_message(message.chat.id,text="/start ==> getting start with this bot !\n\n/help ==> to get help in how to use this bot\n\n/commands ==> to show all commands \n\n/youtube_help ==> to get help in how to use youtube tools\n\n/youtube <VIDEO LINK> ==> to download a youtube video 'mp4 extension'\n\n/youtube_audio <VIDEO LINK> ==> to download youtube video's audio 'mp3 extension'\n\n/instagram ==> download any instagram video or photo (no reels or IGTV)")
@@ -53,7 +53,7 @@ def youtube_downloader(message):
         vide = video.streams.get_highest_resolution().download(filename="Downloaded_by_sirr_b52.mp4")
         vid = open(vide,'rb')
         bot.send_video(message.chat.id,video=vid)
-        bot.reply_to(message,"Download completed Successfully !\nvideo title : {}\nvideo views : {}\nvideo length : '{}' minutes\n\nBy : @sirr_b52".format(video.title,video.views,length))
+        bot.reply_to(message,"Download completed Successfully ✅\n➡️video title : {}\n➡️video views : {}\n➡️video length : '{}' minutes\n\nBy : @sirr_b52".format(video.title,video.views,length))
         vid.close()
         os.remove("Downloaded_by_sirr_b52.mp4")
         print("success")
@@ -69,7 +69,7 @@ def yotube_audio_downloader(message):
         audi = audio.streams.get_highest_resolution().download(filename="Downloaded_by_sirr_b52.mp3")
         aud = open(audi,'rb')
         bot.send_document(message.chat.id,aud)
-        bot.reply_to(message,"Download completed Successfully !\nvideo title : {}\nvideo views : {}\nvideo length : '{}' minutes\n\nBy : @sirr_b52".format(audio.title,audio.views,length))
+        bot.reply_to(message,"Download completed Successfully ✅\n➡️video title : {}\n➡️video views : {}\n➡️video length : '{}' minutes\n\nBy : @sirr_b52".format(audio.title,audio.views,length))
         aud.close()
         os.remove("Downloaded_by_sirr_b52.mp3")
     except:
@@ -79,37 +79,41 @@ def yotube_audio_downloader(message):
 
 @bot.message_handler(commands=["instagram"])
 def instagram(message):
-    url = "https://instagram-media-downloader.p.rapidapi.com/rapid/post.php"
-    postUrl = message.text.replace("/instagram ","")
-    print(postUrl)
-    headers = {
-    'x-rapidapi-host': "instagram-media-downloader.p.rapidapi.com",
-    'x-rapidapi-key': "3e4a585b37msh0d5d14130a89f2ap10380ajsnca63527f890b" # Put Your API Key 
-    }
-    querystring = {"url": postUrl}
+    
+    try:
+        url = "https://instagram-media-downloader.p.rapidapi.com/rapid/post.php"
+        postUrl = message.text.replace("/instagram ","")
+        print(postUrl)
+        headers = {
+        'x-rapidapi-host': "instagram-media-downloader.p.rapidapi.com",
+        'x-rapidapi-key': "3e4a585b37msh0d5d14130a89f2ap10380ajsnca63527f890b" # Put Your API Key 
+        }
+        querystring = {"url": postUrl}
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+        response = requests.request("GET", url, headers=headers, params=querystring)
 
-    textToJson = json.loads(response.text)
+        textToJson = json.loads(response.text)
 
-    postFileUrl = ''
+        postFileUrl = ''
 
-    if 'video' in textToJson:
-        postFileUrl = textToJson["video"]
-    else:
-        postFileUrl = textToJson["image"]
+        if 'video' in textToJson:
+            postFileUrl = textToJson["video"]
+        else:
+            postFileUrl = textToJson["image"]
 
-    reqPostFileUrl = requests.get(postFileUrl)
+        reqPostFileUrl = requests.get(postFileUrl)
 
-    fileEx = reqPostFileUrl.headers['Content-Type'].split('/')[-1].split('.')[0]
-    filename_ = "downloaded by sirr_b52."+str(fileEx)
-    with reqPostFileUrl as rq:
-        with open(filename_, 'wb') as file:
-         file.write(rq.content)
-    botfile = open(filename_,'rb')
-    bot.send_document(message.chat.id,botfile)
-    botfile.close()
-
+        fileEx = reqPostFileUrl.headers['Content-Type'].split('/')[-1].split('.')[0]
+        filename_ = "downloaded by sirr_b52."+str(fileEx)
+        with reqPostFileUrl as rq:
+            with open(filename_, 'wb') as file:
+                file.write(rq.content)
+        botfile = open(filename_,'rb')
+        bot.send_document(message.chat.id,botfile)
+        botfile.close()
+        bot.reply_to(message.chat.id,"Post downloaded successfully ✅")
+    except:
+        bot.reply_to(message.chat.id,text="Sorry .. I cant download this type of posts ❌")
 
 @bot.message_handler(commands=["instagram_help"])
 def instagram_help(message):
